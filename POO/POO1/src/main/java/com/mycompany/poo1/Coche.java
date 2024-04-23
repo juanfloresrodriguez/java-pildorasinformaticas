79f783c3dec6444d3400bdd91bd6f6f22aae8167
juanfloresrodriguez <148755231+juanfloresrodriguez@users.noreply.github.com>
2024-04-23 14:03:52 +0200

    Prueba

---
+++ POO/POO1/src/main/java/com/mycompany/poo1/Coche.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo1;
+
+/**
+ *
+ * @author juane
+ */
+public class Coche {
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+    
+    public Coche(){
+        ruedas=4;
+    }
+}
